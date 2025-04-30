3f1388c0eb5da4067e8f410842c34fc7930bb7ba
Jamir <jamirjuan2@gmail.com>
2025-04-29 21:55:39 -0500

    Ajustar posición de barra de navegación y corregir layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Home from './components/Home';
 import Nosotros from './components/Nosotros';
 import Contactanos from './components/Contactanos';
 import Login from './components/Login';
+import './App.css';
 
 const App = () => (
   <BrowserRouter>
     <NavigationBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/nosotros" element={<Nosotros />} />
-      <Route path="/contactanos" element={<Contactanos />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <div className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/contactanos" element={<Contactanos />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   </BrowserRouter>
 );
 
