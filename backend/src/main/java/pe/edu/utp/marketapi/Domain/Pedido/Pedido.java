51d837bad9a99d4b0d224de016b7245be5f1c239
Jamir <jamirjuan2@gmail.com>
2025-06-03 23:28:52 -0500

    Agregar entidad Pedido con atributos, constructor y método de actualización

---
+++ backend/src/main/java/pe/edu/utp/marketapi/Domain/Pedido/Pedido.java
@@ -0,0 +1,47 @@
+package pe.edu.utp.marketapi.Domain.Pedido;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import pe.edu.utp.marketapi.Domain.Usuario.Usuario;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private Long idPedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Column(name = "fecha_pedido")
+    private Timestamp fechaPedido;
+
+    private double total;
+
+    private boolean estado = true;
+
+    public Pedido(DataRegisterPedido data){
+        this.usuario = Usuario.builder().idUsuario(data.idUsuario()).build();
+        this.fechaPedido = new Timestamp(System.currentTimeMillis());
+        this.estado = true;
+    }
+
+    public void updatePedido(DataUpdatePedido data){
+        if (data.idUsuario() != null) this.usuario = Usuario.builder().idUsuario(data.idUsuario()).build();
+        if (data.total() != null) this.total = data.total();
+    }
+}
