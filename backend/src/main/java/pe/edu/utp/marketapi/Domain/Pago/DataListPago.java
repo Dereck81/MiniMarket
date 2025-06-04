ccb33a91f58838cfdb3e0a84e8850e87fc7163cc
Jamir <jamirjuan2@gmail.com>
2025-06-04 00:44:32 -0500

    Implementar InventarioService con CRUD, filtrado por nombre/categoría, reporte Excel y control de stock bajo

--- backend/src/main/java/pe/edu/utp/marketapi/Domain/Pago/DataListPago.java
+++ backend/src/main/java/pe/edu/utp/marketapi/Domain/Pago/DataListPago.java
@@ -2,7 +2,6 @@ package pe.edu.utp.marketapi.Domain.Pago;
 
 import pe.edu.utp.marketapi.Domain.MetodoPago.DataListMetodoPago;
 import pe.edu.utp.marketapi.Domain.Pedido.DataListPedido;
-import pe.edu.utp.marketapi.Domain.Producto.DataListProducto;
 
 public record DataListPago(
         Long idPago,
